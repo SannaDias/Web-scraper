f66daad5a49a2d9dae40870727b9e3f9abefaddf
SannaDias <sannapdias@gmail.com>
2024-04-20 09:35:14 -0300

    [Web scraper]Endpoints

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const pup = require('puppeteer');
 
 const url = "https://www.mercadolivre.com.br";
-const searchFor = "Mais vendidos";
+const searchFor = "mais vendidos bestseller";
+
+let c = 1;
+const list = [];
+
 
 (async () => {
     try {
@@ -21,10 +25,42 @@ const searchFor = "Mais vendidos";
             page.click('.nav-search-btn')
         ]);
 
-        const produtos = await page.$$eval('.ui-search-result__image > a' , el => el.map(link => link.href));
-        console.log(produtos);
+        const links = await page.$$eval('.ui-search-item__group > a' , el => el.map(link => link.href));
+        
+        
        
-         // await browser.close();
+        //iterar sobre os itens
+        for(const link of links){
+            console.log("pagina", c);
+            await page.goto(link);
+            await page.waitForSelector('.ui-pdp-title'); // mostra apenas a parte da pagina que queremos carregar 
+
+            const title = await page.$eval('.ui-pdp-title', Element => Element.innerText);
+            const price = await page.$eval('.andes-money-amount__fraction', element => element.innerText);
+
+            const seller = await page.evaluate(() => {
+                const el = document.querySelector('.andes-tooltip__trigger');
+                if(!el) return null
+                return el.innerText;
+            })
+
+            //mostrando os elemntos dentro de um obj para leitura
+            const obj ={};
+            obj.title = title;
+            obj.price = price;
+            (seller ? obj.seller = seller : '');
+            obj.link = link;
+
+            list.push(obj);
+
+            console.log(list);
+
+
+            c++
+        }
+       
+        // await page.waitForTimeout(2000);
+        await browser.close();
     } catch (error) {
         console.error("Ocorreu um erro:", error);
     }
